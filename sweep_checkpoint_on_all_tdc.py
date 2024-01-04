e0fc687eef06f66d6a440426ebd337fe25939acf
Blazej Banaszewski <blazejb@graphcore.ai>
2024-01-04 15:14:26 +0000

    fix small bugs

--- sweep_checkpoint_on_all_tdc.py
+++ sweep_checkpoint_on_all_tdc.py
@@ -34,9 +34,8 @@ TDC_BENCHMARKS = [
 ]
 
 
-def create_sweep_and_get_id():
+def create_sweep_and_get_id(sweep_name):
     with open(YAML_FILE_PATH, 'r') as file: sweep_config = yaml.safe_load(file)
-    sweep_name = f"{os.getenv('SWEEP_MODEL_NAME')}|{os.getenv('SWEEP_DATASET')}"
     sweep_config['name'] = sweep_name
     return wandb.sweep(sweep=sweep_config, entity=os.getenv('WANDB_ENTITY'), project=os.getenv('WANDB_PROJECT'))
 
@@ -44,7 +43,8 @@ def get_sweep_status(api):
     return api.sweep(f"{os.getenv('WANDB_ENTITY')}/{os.getenv('WANDB_PROJECT')}/{sweep_id}").state
 
 def get_sweep_id_by_name(api, sweep_name):
-    sweeps = api.sweeps(path=f"{os.getenv('WANDB_ENTITY')}/{os.getenv('WANDB_PROJECT')}")
+    project = api.project(name=os.getenv('WANDB_PROJECT'), entity=os.getenv('WANDB_ENTITY'))
+    sweeps = project.sweeps()
     for sweep in sweeps:
         if sweep.name == sweep_name:
             return sweep.id
@@ -53,10 +53,10 @@ def get_sweep_id_by_name(api, sweep_name):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Sweep Configuration')
-    parser.add_argument('--model_name', type=str, default='10M', help='Name of the sweep model')
-    parser.add_argument('--fingerprints_path', type=str, default='ids_to_fingerprint.pt', help='Path to the fingerprints file')
-    parser.add_argument('--wandb_entity', type=str, default='ogb-lsc-comp', help='W&B entity')
-    parser.add_argument('--wandb_project', type=str, default='scaling_mol_gnns', help='W&B project')
+    parser.add_argument('--model-name', type=str, default='10M', help='Name of the sweep model')
+    parser.add_argument('--fingerprints-path', type=str, default='ids_to_fingerprint.pt', help='Path to the fingerprints file')
+    parser.add_argument('--wandb-entity', type=str, default='ogb-lsc-comp', help='W&B entity')
+    parser.add_argument('--wandb-project', type=str, default='scaling_mol_gnns', help='W&B project')
     args = parser.parse_args()
 
     os.environ['SWEEP_MODEL_NAME'] = args.model_name
@@ -64,7 +64,6 @@ if __name__ == "__main__":
     os.environ['WANDB_ENTITY'] = args.wandb_entity
     os.environ['WANDB_PROJECT'] = args.wandb_project
 
-    wandb.login()
     api = wandb.Api()
 
     for dataset in TDC_BENCHMARKS:
@@ -73,12 +72,12 @@ if __name__ == "__main__":
 
         sweep_id = get_sweep_id_by_name(api, sweep_name)
         if sweep_id is not None:
-            status = get_sweep_status(api, sweep_id)
+            status = get_sweep_status(api)
             if status == 'FINISHED':
                 print(f"Sweep '{sweep_name}' is already finished, moving to the next benchmark.")
                 continue
         else:
-            sweep_id = create_sweep_and_get_id(api, sweep_name)
+            sweep_id = create_sweep_and_get_id(sweep_name)
             print(f"Created sweep with ID {sweep_id} for dataset {dataset}")
 
         wandb.agent(sweep_id)
