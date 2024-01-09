6e542186d30db858b454519cb314000c239d8253
Blazej Banaszewski <blazejb@graphcore.ai>
2024-01-09 12:44:15 +0000

    analzye 5-fold cross validation results

--- analyze_results.py
+++ analyze_results.py
@@ -1,12 +1,13 @@
 import os
 import pickle
 import wandb
+import pandas as pd
 
 # Specify your project and entity (username or team name)
-project_name = 'scaling_mol_gnns'
+project_name = 'scale_mol_gnns_fingerprinting'
 entity_name = 'ogb-lsc-comp'
-pickle_path = 'sweep_results_dict.pickle'
-csv_path = 'sweep_results_table.csv'
+pickle_path = 'results/sweep_results_dict.pickle'
+csv_path = 'results/sweep_results_table.csv'
 
 DEFINITION_OF_BETTER = {
     'mae': min,
@@ -17,28 +18,28 @@ DEFINITION_OF_BETTER = {
 }
 
 BENCHMARKS = {
-    'Caco2_Wang': 'test_mae',
-    'Bioavailability_Ma': 'test_auroc',
-    'Lipophilicity_AstraZeneca': 'test_mae',
-    'Solubility_AqSolDB': 'test_mae',
-    'HIA_Hou': 'test_auroc',
-    'Pgp_Broccatelli': 'test_auroc',
-    'BBB_Martins': 'test_auroc',
-    'PPBR_AZ': 'test_mae',
-    'VDss_Lombardo': 'test_spearman',
-    'CYP2C9_Veith': 'test_auroc',
-    'CYP2D6_Veith': 'test_auroc',
-    'CYP3A4_Veith': 'test_auroc',
-    'CYP2C9_Substrate_CarbonMangels': 'test_auroc',
-    'CYP2D6_Substrate_CarbonMangels': 'test_auroc',
-    'CYP3A4_Substrate_CarbonMangels': 'test_auroc',
-    'Half_Life_Obach': 'test_spearman',
-    'Clearance_Hepatocyte_AZ': 'test_spearman',
-    'Clearance_Microsome_AZ': 'test_spearman',
-    'LD50_Zhu': 'test_mae',
-    'hERG': 'test_auroc',
-    'AMES': 'test_auroc',
-    'DILI': 'test_auroc',
+    'Caco2_Wang':                       'test_mae',
+    'Bioavailability_Ma':               'test_auroc',
+    'Lipophilicity_AstraZeneca':        'test_mae',
+    'Solubility_AqSolDB':               'test_mae',
+    'HIA_Hou':                          'test_auroc',
+    'Pgp_Broccatelli':                  'test_auroc',
+    'BBB_Martins':                      'test_auroc',
+    'PPBR_AZ':                          'test_mae',
+    'VDss_Lombardo':                    'test_spearman',
+    'CYP2C9_Veith':                     'test_auroc',
+    'CYP2D6_Veith':                     'test_auroc',
+    'CYP3A4_Veith':                     'test_auroc',
+    'CYP2C9_Substrate_CarbonMangels':   'test_auroc',
+    'CYP2D6_Substrate_CarbonMangels':   'test_auroc',
+    'CYP3A4_Substrate_CarbonMangels':   'test_auroc',
+    'Half_Life_Obach':                  'test_spearman',
+    'Clearance_Hepatocyte_AZ':          'test_spearman',
+    'Clearance_Microsome_AZ':           'test_spearman',
+    'LD50_Zhu':                         'test_mae',
+    'hERG':                             'test_auroc',
+    'AMES':                             'test_auroc',
+    'DILI':                             'test_auroc'
 }
 
 WANDB_STATES = {
@@ -49,9 +50,8 @@ WANDB_STATES = {
 
 
 
-def best_score_for_sweep(sweep):
-    abs_best_scores = []
-    best_val_loss, best_val_loss_idx = None, None
+def find_best_score_for_sweep(sweep):
+    mean_test_scores, std_test_scores, run_indices = [], [], []
     metric, def_of_better = None, None
     
     for idx, run in enumerate(sweep.runs):
@@ -63,25 +63,22 @@ def best_score_for_sweep(sweep):
             metric = BENCHMARKS[run.config['dataset']]
             def_of_better = DEFINITION_OF_BETTER[metric.split('_')[-1]]
 
-        run_history = run.history()
-        if metric in run_history.keys():
-            abs_best_scores += [def_of_better(run_history[metric])]
-            val_loss = def_of_better(run_history['val_loss'])
-            if best_val_loss is None:
-                best_val_loss = val_loss
-                best_val_loss_idx = idx
-            else:
-                if best_val_loss < val_loss:
-                    best_val_loss = val_loss
-                    best_val_loss_idx = idx
-    
+        run_statistics = run.summary_metrics['statistics']
+        if f"{metric}" in run_statistics.keys():
+            mean_test_scores += [run_statistics[metric]['mean']]
+            std_test_scores += [run_statistics[metric]['std']]
+
     # use appropriate reduction for the metric to get the best score in the sweep
-    abs_best_score = def_of_better(abs_best_scores) if len(abs_best_scores) else 'NaN'
-    if best_val_loss is None:
-        fair_best_score = 'NaN'
+    best_mean_test_score = def_of_better(mean_test_scores) if len(mean_test_scores) else 'NaN'
+
+    # Get the index of best_mean_test_score to find the std_test_score
+    if best_mean_test_score != 'NaN':
+        index_of_best_score = mean_test_scores.index(best_mean_test_score)
+        best_std_test_score = std_test_scores[index_of_best_score]
     else:
-        fair_best_score = def_of_better(sweep.runs[best_val_loss_idx].history()[metric])
-    return abs_best_score, fair_best_score
+        best_std_test_score = 'NaN'
+        
+    return best_mean_test_score, best_std_test_score
 
 def load_results(file_path):
     if os.path.exists(file_path):
@@ -94,25 +91,31 @@ def save_results(results, file_path):
         pickle.dump(results, file)
 
 def save_to_csv(results, csv_path):
-    import pandas as pd
+    # Prepare a list for DataFrame rows
+    data = []
 
-    # Convert results to DataFrame
-    data = [{'model_name': key[0], 'dataset': key[1], **value} for key, value in results.items()]
+    # Iterate through each dataset in BENCHMARKS
+    for dataset in BENCHMARKS.keys():
+        mean_row = {'Metric': 'Mean', 'Dataset': dataset}
+        std_row = {'Metric': 'Std', 'Dataset': dataset}
+
+        # Iterate through results to fill the rows
+        for (model_name, result_dataset), values in results.items():
+            if dataset == result_dataset:
+                mean_row[model_name] = values['mean']
+                std_row[model_name] = values['std']
+
+        data.append(mean_row)
+        data.append(std_row)
+
+    # Convert list to DataFrame
     df = pd.DataFrame(data)
 
-    # Pivot the DataFrame
-    pivot_df = df.pivot(index='dataset', columns='model_name', values=['abs', 'fair'])
-    pivot_df = pivot_df.swaplevel(axis='columns').sort_index(axis='columns')
-    pivot_df.columns = ['_'.join(col).strip() for col in pivot_df.columns.values]
+    # Set the 'Metric' and 'Dataset' columns as a multi-index
+    df.set_index(['Metric', 'Dataset'], inplace=True)
 
-    # Create a DataFrame with all datasets from BENCHMARKS as index
-    all_datasets_df = pd.DataFrame(index=BENCHMARKS.keys())
-
-    # Merge with pivot_df to include all datasets, filling missing ones with NaN
-    complete_df = all_datasets_df.join(pivot_df, how='left')
-
-    print(complete_df)
-    complete_df.to_csv(csv_path)
+    # Save to CSV
+    df.to_csv(csv_path)
 
 
 if __name__ == "__main__":
@@ -139,9 +142,9 @@ if __name__ == "__main__":
             print(f"Sweep state - {sweep.state.lower()} - continuing to the next one")
             continue
 
-        abs_best_score, fair_best_score = best_score_for_sweep(sweep)
-        results[(model_name, dataset)] = {"abs": abs_best_score, "fair": fair_best_score}
-        print(f"{abs_best_score=}, {fair_best_score=}")
+        mean_score, std_score = find_best_score_for_sweep(sweep)
+        results[(model_name, dataset)] = {"mean": mean_score, "std": std_score}
+        print(f"{mean_score=}, {std_score=}")
 
     save_results(results, pickle_path)
     save_to_csv(results, csv_path)
